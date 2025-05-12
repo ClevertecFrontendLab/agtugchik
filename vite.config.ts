bd5c3b4118929b748e6b2dfe250deb5ecf66bc8f
Артур Шведенко <artursvedenko@MacBook-Pro-Artur.local>
2025-05-12 15:34:43 +0300

    refactor(vite): change repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ command }) => ({
-    base: command === 'build' ? '/REPO_NAME/' : '/',
+    base: command === 'build' ? '/agtugchik/' : '/',
     plugins: [react()],
     server: {
         host: true,
